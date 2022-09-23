017bf28f9efdc0660d8c5297824f1e9af24c57de
Jovan Samcovic <>
2022-09-23 12:33:41 +0200

    [Star Wars]
    - Initial setup
    - Added header component (without js)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './style/style.scss';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="app">
+       <Header />
+       <main className="main">
+
+       </main>
+    </div>
+  );
+}
+
+export default App;
