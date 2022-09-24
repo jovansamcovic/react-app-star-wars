f6b77dbace9dd46538fb3c61bab03e5fd1644180
Jovan Samcovic <jsamcovic1996@gmail.com>
2022-09-25 01:44:44 +0200

    [Star Wars] Home page
    - Nav : added hover effect
    - Carousel: Changed background-color on arrow
    - Modified size of social network icons in header and footer
    - Added banner and cards on Home page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,19 @@
 import './Nav.scss';
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="nav__item">Home</li>
-        <li className="nav__item">Starships</li>
-        <li className="nav__item">Actors</li>
+        <li className="nav__item">
+          <NavLink>Home</NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink >Starships</NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink>Actors</NavLink>
+        </li>
       </ul>
     </nav>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,76 @@
 import Carousel from "../components/Carousel/Carousel";
+import Nav from "../components/Nav/Nav";
+import './../style/style.scss';
+
+import card1 from './../img/cards/card1.webp';
+import card2 from './../img/cards/card2.jpg';
+import card3 from './../img/cards/card3.jpg';
+import card4 from './../img/cards/card4.jpg';
 
 const Home = () => {
   return (
+    <>
+      <Nav />
       <Carousel />
+
+      <div className="container">
+
+        <div className="banner">
+          <div className="banner__info">
+            <h3 className="banner__title">A new era of Star Wars</h3>
+            <button className="banner__btn">Explore</button>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className="cards__list">
+            <div className="card">
+              <div className="card__wrap">
+                <img className="card__img" src={card1} alt="error" />
+              </div>
+              <div className="card__info">
+                <div className="card__decal-left"></div>
+                <div className="card__decal-right"></div>
+                <h3>Meet the Ships and Vehicles</h3>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card__wrap">
+                <img className="card__img" src={card2} alt="error" />
+              </div>
+              <div className="card__info">
+                <div className="card__decal-left"></div>
+                <div className="card__decal-right"></div>
+                <h3>Meet the Droids</h3>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card__wrap">
+                <img className="card__img" src={card3} alt="error" />
+              </div>
+              <div className="card__info">
+                <div className="card__decal-left"></div>
+                <div className="card__decal-right"></div>
+                <h3>Meet the Heroes</h3>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card__wrap">
+                <img className="card__img" src={card4} alt="error" />
+              </div>
+              <div className="card__info">
+                <div className="card__decal-left"></div>
+                <div className="card__decal-right"></div>
+                <h3>Meet the Villians</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 };
 
